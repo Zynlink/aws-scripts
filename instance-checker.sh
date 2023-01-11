23df681b2550c22777772c4920346a6cabce6466
ZynLink <116901101+Zynlink@users.noreply.github.com>
2023-01-11 18:15:24 -0500

    Update instance-checker.sh

--- instance-checker.sh
+++ instance-checker.sh
@@ -5,25 +5,13 @@
 # Get all available regions
 regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
 
-# Create the table header
-echo "InstanceId | InstanceType | State | Name | PrivateIpAddress | PublicIpAddress | Region"
-
 for region in $regions
 do
     # List all running instances
-    instances=$(aws ec2 describe-instances --region $region --query "Reservations[*].Instances[*].[InstanceId,InstanceType,State.Name,Tags[?Key=='Name'].Value|[0],PrivateIpAddress,PublicIpAddress]" --output text --filters "Name=instance-state-name,Values=running")
-
-    # Loop through the instances and add them to the table
-    while read -r line; do
-        # Extract the details for each instance
-        instanceId=$(echo $line | awk '{print $1}')
-        instanceType=$(echo $line | awk '{print $2}')
-        state=$(echo $line | awk '{print $3}')
-        name=$(echo $line | awk '{print $4}')
-        privateIp=$(echo $line | awk '{print $5}')
-        publicIp=$(echo $line | awk '{print $6}')
-
-        # Add the instance to the table
-        echo "$instanceId | $instanceType | $state | $name | $privateIp | $publicIp | $region"
-    done <<< "$instances"
+    instances=$(aws ec2 describe-instances --region $region --query "Reservations[*].Instances[*].[InstanceId,InstanceType,State.Name,Tags[?Key=='Name'].Value|[0],PrivateIpAddress,PublicIpAddress]" --output table --filters "Name=instance-state-name,Values=running")
+    if [ -n "$instances" ]; then
+        echo "Instances running in $region"
+        echo "================================="
+        echo "$instances"
+    fi
 done
