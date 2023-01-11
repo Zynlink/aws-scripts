aee285610b27495755254b1210ff7856e4dcff77
ZynLink <116901101+Zynlink@users.noreply.github.com>
2023-01-11 16:56:31 -0500

    Update running-resources.bash

--- running-resources.bash
+++ running-resources.bash
@@ -2,35 +2,30 @@
 
 # AWS CLI must be installed and configured with appropriate credentials
 
-# get all available regions
+# Get all available regions
 regions=$(aws ec2 describe-regions --query "Regions[].RegionName" --output text)
 
-ec2_results=()
-rds_results=()
-elasticache_results=()
-elb_results=()
-asg_results=()
+# Store the results in an array
+results=()
 
 for region in $regions
 do
-    echo "Checking resources in region: $region"
-    echo "======================="
-
-    # list all running ec2 instances
-    ec2_output=$(aws ec2 describe-instances --region $region --query "Reservations[*].Instances[*].[InstanceId,InstanceType,State.Name]" --output text --filters "Name=instance-state-name,Values=running")
-    if [ -z "$ec2_output" ]; then
-        ec2_results+=("None")
-    else
-        ec2_results+=("$ec2_output")
+    # List all running instances
+    output=$(aws ec2 describe-instances --region $region --query "Reservations[*].Instances[*].[InstanceId,InstanceType,State.Name,Tags[?Key=='Name'].Value|[0],PrivateIpAddress,PublicIpAddress]" --output text --filters "Name=instance-state-name,Values=running")
+    if [ -n "$output" ]; then
+        results+=("$output")
     fi
+done
 
-    # list all running RDS instances
-    rds_output=$(aws rds describe-db-instances --region $region --query "DBInstances[*].[DBInstanceIdentifier,DBInstanceClass,Engine,DBInstanceStatus]" --output text --filters "Name=DBInstanceStatus,Values=available")
-    if [ -z "$rds_output" ]; then
-        rds_results+=("None")
-    else
-        rds_results+=("$rds_output")
-    fi
+# Filter the results by tag name
+if [ -n "$1" ]; then
+    results=($(echo "${results[@]}" | grep -i "$1"))
+fi
 
-    # list all running elasticache instances
-    elasticache_output=$(aws elasticache describe-cache-clusters --region $region --query "CacheClusters[*].[CacheClusterId,Cache
+# check if the results is empty if yes display None
+if [ ${#results[@]} -eq 0 ]; then
+    echo "None"
+else
+    # Display the results in a table format
+    echo "${results[@]}" | column -t
+fi
