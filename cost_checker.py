cfe8290cbcd296429a46b2281a7a77a74c1afae0
ZynLink <116901101+Zynlink@users.noreply.github.com>
2023-01-13 11:23:04 -0500

    Update cost_checker.py

--- cost_checker.py
+++ cost_checker.py
@@ -8,13 +8,12 @@ response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Valu
 
 # Get the cost of each instance
 total_cost = 0
-pricing = boto3.client('pricing')
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         instance_type = instance['InstanceType']
         region = instance['Placement']['AvailabilityZone'][:-1]
-        instance_data = pricing.describe_instance_types(InstanceTypes=[instance_type])
-        hourly_rate = instance_data['PriceList'][0]['terms']['OnDemand'][instance_type]['priceDimensions']['pricePerUnit']['USD']
+        instance_data = ec2.describe_instance_types(InstanceTypes=[instance_type])
+        hourly_rate = instance_data['InstanceTypes'][0]['Price']['USD']
         total_cost += hourly_rate
 
 print(f'The total cost of running instances is ${total_cost}')
