4a0465e97f4259453c9674715dc717a11a3ca641
ZynLink <116901101+Zynlink@users.noreply.github.com>
2023-01-13 09:49:24 -0500

    Update cost_checker.py

--- cost_checker.py
+++ cost_checker.py
@@ -12,7 +12,8 @@ for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         instance_type = instance['InstanceType']
         region = instance['Placement']['AvailabilityZone'][:-1]
-        price_data = boto3.client('pricing', region_name=region).get_products(
+        pricing = boto3.client('pricing', region_name=region)
+        price_data = pricing.get_products(
             ServiceCode='AmazonEC2',
             Filters=[
                 {'Type': 'TERM_MATCH', 'Field': 'instanceType', 'Value': instance_type},
@@ -20,7 +21,7 @@ for reservation in response['Reservations']:
                 {'Type': 'TERM_MATCH', 'Field': 'tenancy', 'Value': 'Shared'}
             ]
         )
-        price = price_data['PriceList'][0]['terms']['OnDemand'][instance_type]['priceDimensions']['pricePerUnit']['USD']
+        price = float(price_data['PriceList'][0]['price']['USD'])
         total_cost += price
 
 print(f'The total cost of running instances is ${total_cost}')
